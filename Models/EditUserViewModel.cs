a5f003129d2ee6bdda10b7b1533ba8637fe3836b
hgffghh <ghfjdghfjd3@gmail.com>
2020-11-28 20:07:19 +0100

    rejestracja, logowanie, role

---
+++ Models/EditUserViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GasStation.Models
+{
+    public class EditUserViewModel
+    {
+        public EditUserViewModel()
+        {
+            Roles = new List<string>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nazwa użytkownika")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Nazwisko")]
+        public string Surname { get; set; }
+
+        [Required]
+        public string PESEL { get; set; }
+
+        public IList<string> Roles { get; set; }
+
+    }
+}
