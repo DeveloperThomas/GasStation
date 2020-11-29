2a4236babcc100767d5a704cd1cee97b90df1f5a
Tomasz Zawistowski <to.zawistowski@gmail.com>
2020-11-29 17:25:34 +0100

    Updated transaction create view

---
+++ Models/ModelsView/TransactionProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GasStation.Models.ModelsView
+{
+    public class TransactionProduct
+    {
+        public Product Product { get; set; }
+        public float Amount { get; set; }
+
+        public TransactionProduct(Product product, float amount)
+        {
+            this.Product = product;
+            this.Amount = amount;
+        }
+    }
+}
